3bb6c6b324d1897b19f42fe689f173426cff3978
Victor Deon <victorhad@gmail.com>
2025-05-14 20:46:13 -0300

    Arrumando o layout do requests

--- Sources/NotebookTerminalApp/Library/ThirdPartyLibraries.swift
+++ Sources/NotebookTerminalApp/Library/ThirdPartyLibraries.swift
@@ -38,7 +38,21 @@ struct RequestCommands: ParsableCommand {
     @OptionGroup var common: CommonOptions
 
     func run() throws {
-        thirdPartyLibraryRunner()
+        let login = Login(email: "test@test.test", password: "testPassword")
+        let semaphore = DispatchSemaphore(value: 0)
+        var status_code1: Int? = 0
+        var status_code2: Int? = 0
+
+        Task {
+            status_code1 = await MakeRequest.get()
+            status_code2 = await MakeRequest.post(login: login)
+            semaphore.signal()
+        }
+        
+        // Bloqueia até signal()
+        semaphore.wait()
+        print(status_code1!)
+        print(status_code2!)
     }
 }
 
@@ -47,45 +61,35 @@ struct Login: Encodable {
     let password: String
 }
 
-func makeRequest(login: Login) async -> Int? {
-    let headers: HTTPHeaders = [
-        "Authorization": "Basic VXNlcm5hbWU6UGFzc3dvcmQ=",
-        "Accept": "application/json"
-    ]
-    
-    let postResponse = await AF.request(
-        "https://httpbin.org/post",
-        method: .post,
-        parameters: login,
-        encoder: JSONParameterEncoder.default,
-        headers: headers
-    ).serializingData().response
+struct MakeRequest {
+    static func get() async -> Int? {
+        let getResponse = await AF.request("https://httpbin.org/get")
+            .serializingData()
+            .response
+        
+        debugPrint(getResponse)
 
-    debugPrint(postResponse)
-    
-    let getResponse = await AF.request("https://httpbin.org/get")
-        .serializingData()
-        .response
-    
-    debugPrint(getResponse)
-
-    return getResponse.response?.statusCode
-}
-
-func thirdPartyLibraryRunner() {
-    let login = Login(email: "test@test.test", password: "testPassword")
-    let semaphore = DispatchSemaphore(value: 0)
-    var resultado: Int? = 0
-
-    Task {
-        resultado = await makeRequest(login: login)
-        semaphore.signal()
+        return getResponse.response?.statusCode
     }
     
-    // Bloqueia até signal()
-    semaphore.wait()
-    debugPrint(resultado!)
-    
+    static func post(login: Login) async -> Int? {
+        let headers: HTTPHeaders = [
+            "Authorization": "Basic VXNlcm5hbWU6UGFzc3dvcmQ=",
+            "Accept": "application/json"
+        ]
+        
+        let postResponse = await AF.request(
+            "https://httpbin.org/post",
+            method: .post,
+            parameters: login,
+            encoder: JSONParameterEncoder.default,
+            headers: headers
+        ).serializingData().response
+
+        debugPrint(postResponse)
+        
+        return postResponse.response?.statusCode
+    }
 }
 
 
